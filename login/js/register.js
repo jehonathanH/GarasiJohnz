a5a2baf8f4ce49d7793cab74d1c9401154c2fdbe
jehonathanH <jehonathan1103@gmail.com>
2026-05-18 18:46:54 +0700

    Update register.js

--- login/js/register.js
+++ login/js/register.js
@@ -1,24 +1,42 @@
-document.getElementById("registerForm").addEventListener("submit", async function(e) {
+document.getElementById("registerForm")
+.addEventListener("submit", async function(e){
+
     e.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+    const username =
+    document.getElementById("username").value.trim();
+
+    const email =
+    document.getElementById("email").value.trim();
+
+    const password =
+    document.getElementById("password").value.trim();
 
     try {
 
-        const res = await fetch("https://herisusanta.my.id/javalogin/api/auth.php", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body:
-    `action=register&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-});
+        const res = await fetch(
+            "https://herisusanta.my.id/javalogin/api/auth.php",
+            {
+                method: "POST",
+
+                headers: {
+                    "Content-Type":
+                    "application/x-www-form-urlencoded"
+                },
+
+                body:
+                `action=register&username=${encodeURIComponent(username)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
+            }
+        );
+
         const data = await res.json();
 
-        if (data.status === "success") {
+        console.log(data);
+
+        if(data.status === "success"){
 
             alert("Registrasi berhasil!");
+
             window.location.href = "index.html";
 
         } else {
@@ -27,8 +45,12 @@ document.getElementById("registerForm").addEventListener("submit", async functio
 
         }
 
-    } catch (err) {
+    } catch(err){
+
         console.error(err);
+
         alert("Server error");
+
     }
+
 });
